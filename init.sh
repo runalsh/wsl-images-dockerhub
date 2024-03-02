7e6d607dfa5720bcce5e965e37037260e22b6dc4
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-02 15:53:59 +0300

    debian + ubuntu

---
+++ init.sh
@@ -0,0 +1,3 @@
+timedatectl set-timezone Europe/Moscow
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
---
+++ install-packages.sh
@@ -0,0 +1,11 @@
+apt-get update 
+apt-get install -qq -y --no-install-recommends \
+    nmon \
+    nano \
+    net-tools \
+    jq \
+    curl \
+    wget \
+    jq \
+    sudo \
+ && rm -rf /var/lib/apt/lists/*
