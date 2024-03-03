243521b6d26e9874cc6eb070b65cdf6670ce7c49
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-03 18:48:23 +0300

    test9

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-# echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-# echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-# sysctl -p
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+sysctl -p
 mkdir -p /root/.ssh/
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByxq8vrVcDlFlXlNUizeE/T2leMF0k6JhaXrdeUtZZj ed25519-key-20240302" >> /root/.ssh/authorized_keys
 #sed -ie '0,/#PermitRootLogin prohibit-password/s/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
