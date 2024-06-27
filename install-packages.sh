5f68c510da9be782bdd645dfbd21ddb5a8e3e902
runalsh <21105673+runalsh@users.noreply.github.com>
2024-06-28 00:37:59 +0300

    fix6

--- install-packages.sh
+++ install-packages.sh
@@ -21,14 +21,14 @@ apt-get install -y --no-install-recommends --no-install-suggests \
     coreutils  \
     telnet \
     gpg \
-    grep
-        # locales \
+    grep \
+    locales 
 echo Europe/Moscow > /etc/timezone
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
 echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
 echo "export LANG=en_US.UTF-8" >> ~/.bashrc
 echo "export LANGUAGE=en_US.UTF-8" >> ~/.bashrc
-# locale-gen en_US.UTF-8
+locale-gen en_US.UTF-8
 echo LANG=en_US.UTF-8 >> /etc/default/locale
 apt-get autoremove --yes
 apt-get clean all
